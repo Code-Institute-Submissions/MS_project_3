b46eefdd5e4a08d26f4916144013dd5ab8f674e1
Elena-Pakhmurskaia <elenapakhmurskaia@gmail.com>
2020-11-21 18:35:32 +0000

    added my_sightings page which will later read the added values

--- app.py
+++ app.py
@@ -46,9 +46,48 @@ def register():
         # register the new user 
         session["user"] = request.form.get("username").lower()
         flash("Registration Successful!")
+        return redirect(url_for('my_sightings', username=session["user"]))
     return render_template("register.html")
-    
 
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        # check if username exists in db
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            # ensure hashed password matches user input
+            if check_password_hash(
+                existing_user["password"], request.form.get("password")):
+                    session["user"] = request.form.get("username").lower()
+                    flash("Welcome, {}".format(
+                        request.form.get("username")))
+                    return redirect(url_for(
+                        'my_sightings', username=session["user"]))
+
+            else:
+                # invalid password match
+                flash("Incorrect Username and/or Password")
+                return redirect(url_for("login"))
+
+        else:
+            # username doesn't exist
+            flash("Incorrect Username and/or Password")
+            return redirect(url_for("login"))
+
+    return render_template("login.html")
+
+
+@app.route("/my_sightings/<username>",methods=["GET", "POST"])
+def my_sightings(username):
+        # grab the session user's username from db
+    username = mongo.db.users.find_one(
+        {"username": session["user"]})["username"]
+    return render_template("my_sightings.html", username=username)
+
+    
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
