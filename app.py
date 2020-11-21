3b77b803b5ca5cb906cdb2dc4511b77c6fbcc173
Elena-Pakhmurskaia <elenapakhmurskaia@gmail.com>
2020-11-21 17:46:31 +0000

    registration functionality set up, mongobd read the inputs; defence for password and name enabled

--- app.py
+++ app.py
@@ -28,6 +28,24 @@ def index():
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
+    if request.method == "POST":
+        # check if username already exists
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            flash("Username already exists")
+            return redirect(url_for("register"))
+
+        register = {
+            "username": request.form.get("username").lower(),
+            "password": generate_password_hash(request.form.get("password"))
+        }
+        mongo.db.users.insert_one(register)
+
+        # register the new user 
+        session["user"] = request.form.get("username").lower()
+        flash("Registration Successful!")
     return render_template("register.html")
     
 
